8ab41950b1c025d071d0f75c0456bdb389221410
Dev-Luke-A <landrews540@gmail.com>
2024-02-26 12:54:05 +0000

    Merged into master

--- sustainability/admin.py
+++ sustainability/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from .models import PlantOfTheDay, Plant, Rarity, Card
 from .models import PlantOfTheDay, Plant, Card, Rarity
 
 admin.site.register(PlantOfTheDay)
 admin.site.register(Plant)
-admin.site.register(Card)
 admin.site.register(Rarity)
+admin.site.register(Card)
+
 # Register your models here.
