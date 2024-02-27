a1010c82732c9cc5fd20fd2f82edba497a73a345
Dev-Luke-A <landrews540@gmail.com>
2024-02-27 16:05:27 +0000

    Show cards that the user has and doesn't have

--- sustainability/admin.py
+++ sustainability/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import PlantOfTheDay, Plant, Rarity, Card
+from .models import PlantOfTheDay, Plant, Rarity, Card, UsersCard
 from .models import PlantOfTheDay, Plant, Card, Rarity
 
 admin.site.register(PlantOfTheDay)
 admin.site.register(Plant)
 admin.site.register(Rarity)
 admin.site.register(Card)
+admin.site.register(UsersCard)
 
 # Register your models here.
