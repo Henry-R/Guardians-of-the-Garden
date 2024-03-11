5a993ef1bc66cc323f29baad6279cbeaf7746d08
maclmatt <mayaclmatthews@gmail.com>
2024-03-11 20:08:28 +0000

    Fixed user signup issues

--- registration/forms.py
+++ registration/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+
+from sustainability.models import Userprofile
 
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=150, help_text='Enter a valid email address.')
 
     class Meta:
-        model = User
+        model = Userprofile
         fields = ['username', 'email', 'password1', 'password2']
