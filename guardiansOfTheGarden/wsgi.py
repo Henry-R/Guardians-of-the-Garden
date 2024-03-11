4116043c34ac0c483d4965c9e98fecc603715160
isabellewalford <issywalf@gmail.com>
2024-03-11 09:49:45 +0000

    changing name

--- guardiansOfTheGarden/wsgi.py
+++ guardiansOfTheGarden/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for saltedCoffee project.
+WSGI config for guardiansOfTheGarden project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saltedCoffee.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guardiansOfTheGarden.settings')
 
 application = get_wsgi_application()
