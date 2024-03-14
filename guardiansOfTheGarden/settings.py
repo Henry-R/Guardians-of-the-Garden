f085a2db0105d2d4ab6d1c16fbe5e9489dd1d422
maclmatt <mayaclmatthews@gmail.com>
2024-03-14 15:47:38 +0000

    Add change details view

--- guardiansOfTheGarden/settings.py
+++ guardiansOfTheGarden/settings.py
@@ -121,10 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = []
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 LOGIN_URL = '/registration/login'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- guardiansOfTheGarden/urls.py
+++ guardiansOfTheGarden/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
+
+from guardiansOfTheGarden import settings
 
 urlpatterns = [
     path('', include('sustainability.urls')),
@@ -23,3 +26,4 @@ urlpatterns = [
     path('sustainability/', include("sustainability.urls")),
     path('registration/', include("registration.urls")),
 ]
+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
