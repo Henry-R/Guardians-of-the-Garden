e0dd5862619fcb4a53a47e020b15b16c51bff659
HRW221 <hrw221@exeter.ac.uk>
2024-03-16 10:25:03 +0000

    Removed commented out static config settings

--- guardiansOfTheGarden/settings.py
+++ guardiansOfTheGarden/settings.py
@@ -111,20 +111,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR / 'static']
 LOGIN_URL = '/registration/login'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Railway static stuff
 STATIC_URL = 'static/'
